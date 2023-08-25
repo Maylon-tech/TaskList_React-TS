4cdbbe654ff728ea112df42c5ea7b77b647eb6bb
Maylon-tech <minatokiroisen77@gmail.com>
2023-08-25 22:18:09 +0900

    create and prepare styled Components file

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import { Container, Area, Header } from './App.styles'
+import { Item } from './types/Item'
 
-function App() {
+
+
+const App = () =>  {
+  const [list, setList] = useState<Item[]>([])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container>
+      <Area>
+        <Header>Lista de Tarefa</Header>
+      </Area>
+    </Container>
+  )
 }
 
 export default App;
